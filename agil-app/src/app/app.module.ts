60c05a671adfeb73da6ed9ea8a0cdb9ad4621c48
Lucas Gemin <lucasgemin@gmail.com>
2017-07-25 23:58:48 -0300

    Commit inicial boilerplate

--- agil-app/src/app/app.module.ts
+++ agil-app/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+
+import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ agil-app/src/app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Hulk'},
+  {id: 12, name: 'Spider Man'},
+  {id: 13, name: 'Super Man'},
+  {id: 14, name: 'Thor'},
+  {id: 15, name: 'Iron Man'},
+  {id: 16, name: 'Ant Man'},
+  {id: 17, name: 'Batman'},
+  {id: 18, name: 'DeadPool'}
+]
